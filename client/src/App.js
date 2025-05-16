3471865f3a333c9d94140347c824e1a0d278bcf1
Rasibul <rasibul179@gmail.com>
2025-05-16 23:52:54 +0600

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`
+
+
 
 function App() {
+  const { loading, data } = useQuery(query);
+  if (loading) return <p>Loading...</p>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(data)}
     </div>
   );
 }
